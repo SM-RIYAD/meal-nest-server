e0fbf710d8604a0ba6d51870634c1f4b8c283ea9
SM RIYAD <smriyad0185@gmail.com>
2023-11-25 11:41:00 +0600

    added user related api

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
+app.use(
+    cors({
+      origin: ["http://localhost:5173","https://job-sphere.web.app","https://job-sphere.firebaseapp.com"],
+      credentials: true,
+    })
+  );
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wacbf1n.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     serverApi: {
@@ -29,11 +36,14 @@ const client = new MongoClient(uri, {
   dbConnect();
 
   const allMealsCollection = client.db("mealNest").collection("allmeals");
+  const userCollection = client.db("mealNest").collection("users");
+
+
 app.get("/", (req, res) => {
     res.send("meal nest server is running");
   });
   
-
+///getting all meals api
   app.get("/meals", async (req, res) => {
     // console.log(req.query.email);
     // console.log("token owner info", req.user);
@@ -49,8 +59,46 @@ app.get("/", (req, res) => {
     res.send(result);
   });
 
+///adding user api
+app.post('/users', async (req, res) => {
+    const user = req.body;
+    // insert email if user doesnt exists: 
+    // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+    console.log("user for debug req",user)
+    const query = { email: user?.email }
+    const existingUser = await userCollection.findOne(query);
+    if (existingUser) {
+      return res.send({ message: 'user already exists', insertedId: null })
+    }
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  });
+
+///checking admin api
+
+app.get('/users/admin/:email', async (req, res) => {
+    const email = req.params.email;
+
+    // if (email !== req.decoded.email) {
+    //   return res.status(403).send({ message: 'forbidden access' })
+    // }
+
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    let admin = false;
+    if (user) {
+      admin = user?.role === 'admin';
+    }
+    res.send({ admin });
+  })
+
+///get all the users 
 
 
+app.get('/users', async (req, res) => {
+  const result = await userCollection.find().toArray();
+  res.send(result);
+});
 
 
   app.listen(port, () => {
